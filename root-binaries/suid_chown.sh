cad578e0d7c2373bb7e28cab2b34808936674c85
Akash Pandey <75413146+1337-L3V1ATH0N@users.noreply.github.com>
2024-01-24 00:29:19 -0500

    Update suid_chown.sh

--- root-binaries/suid_chown.sh
+++ root-binaries/suid_chown.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -en "\n[!] Files Ownership you want to change(specify full path): "
+echo -en "\n[*] Files Ownership you want to change(specify full path): "
 read LFILE
 
 chown $(id -un):$(id -gn) $LFILE
